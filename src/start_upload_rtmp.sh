ef5c1583cf7eaf2235a3847aa7f00b10bb9a6f51
EC2 Default User <ec2-user@ip-172-16-39-29.cn-north-1.compute.internal>
2021-04-05 11:58:58 +0000

    update rtmp

--- src/start_upload_rtmp.sh
+++ src/start_upload_rtmp.sh
@@ -1,2 +1,2 @@
-ffmpeg -f v4l2 -r 24 -video_size vga -pix_fmt yuv420p12be -i /dev/video0 -b:v 500k -c:v h264_omx -preset ultrafast -an -f flv rtmp://52.80.130.52/live/123456
-
+# ffmpeg -f v4l2 -r 24 -video_size vga -pix_fmt yuv420p12be -i /dev/video0 -b:v 500k -c:v h264_omx -preset ultrafast -an -f flv rtmp://52.80.130.52/live/123456
+ffmpeg -f v4l2 -r 8 -video_size vga -pix_fmt yuv420p12be -i /dev/video0 -b:v 500k -c:v h264_omx -preset ultrafast -an -f flv rtmp://52.81.133.221:1935/live/123456
